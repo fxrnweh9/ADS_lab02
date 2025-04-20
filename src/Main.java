0804ff3d12c7e5384bc293362137ed04e9ac829e
fxrnweh9 <olzo2377@gmail.com>
2025-04-20 12:45:14 +0500

    commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import Test.TestMyArrayList;
+import models.MyArrayList;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        run(new Scanner(System.in));
+    }
+
+    public static void run(Scanner in) {
+
+        while (true) {
+            System.out.println("[1] - Run MyArrayList");
+            System.out.println("[2] - Run MyLinkedList");
+            System.out.println("[3] - Run MyStack");
+            System.out.println("[4] - Run MyHashMap");
+            System.out.println("[5] - Run MyTreeMap");
+            System.out.println("[0] - Exit");
+
+            int choice = in.nextInt();
+
+            switch (choice) {
+                case 1:
+                    TestMyArrayList.run();
+                    System.out.println();
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("invalid input");
+
+            }
+
+
+
+        }
+    }
+}
---
+++ src/Test/TestMyArrayList.java
@@ -0,0 +1,60 @@
+package Test;
+
+import models.MyArrayList;
+import java.util.Comparator;
+
+public class TestMyArrayList {
+
+
+    public static void run(){
+
+        MyArrayList<Integer> list = new MyArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+
+        System.out.println("integers: ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+
+        list.add(1, 40);
+        System.out.println("After adding 40 at index 1");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+
+
+        list.remove(2);
+        System.out.println("After removing 2 at index 2");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        list.add(10);
+        System.out.println("First index of 10: " + list.indexOf(10));
+        System.out.println("Last index of 10: " + list.lastIndexOf(20));
+
+
+
+        System.out.println("Does 1 exist? " + list.exists(1));
+        System.out.println("Does 1947 exist? " + list.exists(1947));
+
+
+
+        list.sort(Comparator.naturalOrder());
+        System.out.println("After sort: ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+
+        list.clear();
+        System.out.println("After clearing the integers size is: " + list.size());
+    }
+
+}
